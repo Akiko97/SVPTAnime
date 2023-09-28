d949b648bacc3270c5a36412980c18d940e567b1
Gabriel Zhong <37476779+Akiko97@users.noreply.github.com>
2023-09-29 02:40:19 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import Node from './lib/Node.svelte'
+</script>
+
+<main>
+  <div class="card">
+    <Counter />
+    <Node id="node1" />
+    <Node id="node2" />
+    <Node id="node3" />
+  </div>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Node.svelte
@@ -0,0 +1,20 @@
+<script>
+    import anime from 'animejs'
+    export let id
+    let translateX = 100
+    const clicked = () => {
+        anime({
+            targets: `#${id}`,
+            translateX: translateX,
+            rotate: '1turn',
+            duration: 800
+        })
+        translateX += 100
+    }
+</script>
+
+<div {id} role="button" tabindex="0" on:click={clicked} on:keyup={() => {}}>
+    <svg width="100" height="100">
+        <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+    </svg>
+</div>
